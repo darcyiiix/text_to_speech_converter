7b3b09e6d7d6c7fd79e6ec0b1b68fdd457b8a31b
darcyiiix <chillfrags98@gmail.com>
2023-08-12 16:19:46 +0500

    initial commit

---
+++ text-to-speech.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// creating a speech instance
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+const button = document.querySelector("button");
+
+// Adding Event Listener on play button
+
+button.addEventListener('click', () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices()
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => {
+        (voiceSelect.options[i] = new Option(voice.name, i))
+    });
+};
+
+voiceSelect.addEventListener('change', () => {
+    speech.voice = voices[voiceSelect.value];
+})
